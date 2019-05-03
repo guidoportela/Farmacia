27c89f9327c63f0e56b6d73e858a1b5e4f11c7b1
guidoportela <guidoportela@gmail.com>
2019-05-02 23:49:36 -0300

    Adicionando os metodos salvar, buscar, listar, editar e excluir da tela cidades

---
+++ src/main/java/br/com/guido/farmacia/bean/CidadeBean.java
@@ -0,0 +1,118 @@
+package br.com.guido.farmacia.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.com.guido.farmacia.dao.CidadeDAO;
+import br.com.guido.farmacia.dao.EstadoDAO;
+import br.com.guido.farmacia.domain.Cidade;
+import br.com.guido.farmacia.domain.Estado;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class CidadeBean implements Serializable {
+
+	private Cidade cidade;
+	private List<Cidade> cidades;
+	private List<Estado> estados;
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	@PostConstruct // chamado depois q a tela for criado
+	public void listar() {
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidades = cidadeDAO.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as cidades");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novaCidade() {
+		try {
+			cidade = new Cidade();
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao gerar uma nova cidade");
+			erro.printStackTrace();
+		}
+	}
+
+	public void salvar() {
+		try {
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.merge(cidade);
+
+			cidade = new Cidade();
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar(); // LIMPAR COMBO DE ESTADOS
+			cidades = cidadeDAO.listar(); // LIMPAR TABELA DE CIDADES NA LISTAGEM
+			Messages.addGlobalInfo("Cidade salva com sucesso");
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar a cidade");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			cidade = (Cidade) evento.getComponent().getAttributes().get("cidadeSelecionada");
+
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.excluir(cidade);
+			cidades = cidadeDAO.listar(); // atualiza a lista
+			Messages.addGlobalInfo("Cidade removida com sucesso");
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar remover a cidade");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		try {
+			cidade = (Cidade) evento.getComponent().getAttributes().get("cidadeSelecionada"); // captura o estado
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar selecionar uma cidade");
+			erro.printStackTrace();
+		}
+	}
+}
