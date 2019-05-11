de954c0a0629a08e5f652c1ab04cee4a158b5e95
guidoportela <guidoportela@gmail.com>
2019-05-10 21:10:47 -0300

    Atualizando a caixa

---
+++ src/main/java/br/com/guido/farmacia/bean/LoginBean.java
@@ -0,0 +1,58 @@
+package br.com.guido.farmacia.bean;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+
+import br.com.guido.farmacia.dao.UsuarioDAO;
+import br.com.guido.farmacia.domain.Pessoa;
+import br.com.guido.farmacia.domain.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	private Usuario usuario;
+	private Usuario usuarioLogado;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}
+
+	public void setUsuarioLogado(Usuario usuarioLogado) {
+		this.usuarioLogado = usuarioLogado;
+	}
+
+	@PostConstruct
+	public void iniciar() {
+		usuario = new Usuario();
+		usuario.setPessoa(new Pessoa());
+	}
+
+	public void autentificar() {
+		try {
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarioLogado = usuarioDAO.autenticar(usuario.getPessoa().getCpf(), usuario.getSenha());
+
+			if (usuarioLogado == null) {
+				Messages.addGlobalError("CPF e/ou senha incorretas");
+				return;
+			}
+			Faces.redirect("./pages/principal.xhtml");
+		} catch (IOException erro) {
+			Messages.addGlobalError(erro.getMessage());
+		}
+	}
+}
--- src/test/java/br/com/guido/farmacia/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/guido/farmacia/dao/UsuarioDaoTest.java
@@ -1,5 +1,6 @@
 package br.com.guido.farmacia.dao;
 
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -24,7 +25,10 @@ public class UsuarioDaoTest {
 			Usuario usuario = new Usuario();
 			usuario.setAtivo(true);
 			usuario.setPessoa(pessoa);
-			usuario.setSenha("1234");
+			usuario.setSenhaSemCriptografia("1234");
+
+			SimpleHash hash = new SimpleHash("md5", usuario.getSenhaSemCriptografia());
+			usuario.setSenha(hash.toHex());
 			usuario.setTipo('A');
 
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
